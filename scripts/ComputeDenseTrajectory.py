b98a1cf3e2565cb52cf89ed776ae25d59f528d3e
sbondada <sbondada@buffalo.edu>
2014-01-20 19:23:03 -0500

    corrected the code path structure and documented the code

--- scripts/ComputeDenseTrajectory.py
+++ scripts/ComputeDenseTrajectory.py
@@ -1,30 +1,44 @@
 import os
 import subprocess
 
+''' 
+this file is a script which based on the video locations creates the dense trajectory features
+'''
+
 def computeDenseTrajectory(location2save):
+    '''
+    here we recursively traverse the folders from the given location and store the required parameters like
+    dirpath- directory path     
+    dirnames-includes a list of directory name alone not the path
+    filenames-includes the list of the name of the files, not the paths
+    '''
+
     for dirpath, dirnames, filenames in os.walk(location2save):
         print "dir path"+dirpath
         print "len of the dir"+str(len(dirnames))
         splitstr=str.split(dirpath,"/")
         #print "length of split str"+str(len(splitstr))
         print splitstr
+        #iterate the file list
         for files in filenames :
             print files
-            if len(splitstr)>4:
-                p=subprocess.Popen(['./../release/DenseTrack',os.path.join(dirpath,files)],stdout=subprocess.PIPE)
+            #based on the no of directories depth a particular part of the if is executed
+            # 9 is till the videos folder starting from "" to videos 10th folder is action folder
+            if len(splitstr)>9:
+                p=subprocess.Popen(['/home/kaushal/Documents/projects/dense_trajectory_and_codebook/release/DenseTrack',os.path.join(dirpath,files)],stdout=subprocess.PIPE)
                 (output,error)=p.communicate()
                 #print output
-                f=open('../data/results/'+splitstr[2]+'/'+splitstr[4]+'/'+files+'.txt','w');
+                f=open('/home/kaushal/Documents/projects/dense_trajectory_and_codebook/data/results/'+splitstr[7]+'/'+splitstr[9]+'/'+files+'.txt','w');
                 f.write(output);
                 f.close
             else:
-                p=subprocess.Popen(['./../release/DenseTrack',os.path.join(dirpath,files)],stdout=subprocess.PIPE)
+                p=subprocess.Popen(['/home/kaushal/Documents/projects/dense_trajectory_and_codebook/release/DenseTrack',os.path.join(dirpath,files)],stdout=subprocess.PIPE)
                 (output,error)=p.communicate()
                 #print output
-                f=open('../data/results/'+splitstr[2]+'/'+files+'.txt','w');
+                f=open('/home/kaushal/Documents/projects/dense_trajectory_and_codebook/data/results/'+splitstr[7]+'/'+files+'.txt','w');
                 f.write(output);
                 f.close
             #print files
 
 if __name__=='__main__':
-    computeDenseTrajectory('../data/KTH/videos') 
+    computeDenseTrajectory('/home/kaushal/Documents/projects/dense_trajectory_and_codebook/data/MSR1/videos') 
