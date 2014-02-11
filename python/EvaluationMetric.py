44af449c9a427f8db47a8d8e4641f8143847f17f
sbondada <sbondada@buffalo.edu>
2014-02-10 20:31:59 -0500

    modified the code so that the get trajectory code would directly return the code we wanted

--- python/EvaluationMetric.py
+++ python/EvaluationMetric.py
@@ -1,4 +1,7 @@
 import os
+from scipy.cluster.vq import kmeans,vq
+from numpy import array
+import pickle
 import tables
 import subprocess
 import sys
@@ -16,17 +19,19 @@ def getTrajectories(filepath,startframe,endframe,trajectoylength,x,y,width,heigh
     the condition which is used to select the bounded trajectories from the dataset is 
     we select those trajectories where atleast half of trajectory path is inside the bounding volume
     '''
-
+    trajectoryList=[]
     for line in f:
         linesplit=str(line).split('\t')
+        linesplit=[float(x) for x in linesplit[:] if x!='\n']
+
         #condition to check if the trajectory is in the given start and end frame or not
-        if int(math.ceil(float(linesplit[1])))>=startframe and int(math.ceil(float(linesplit[1])))<=endframe:
+        if int(math.ceil(linesplit[1]))>=startframe and int(math.ceil(linesplit[1]))<=endframe:
             condition=True
             inc=0
             #looping the trajectory path
             for i in range(11,40,2):
-                x=float(linesplit[i])
-                y=float(linesplit[i+1])
+                x=linesplit[i]
+                y=linesplit[i+1]
                 #checking if the the trajectory at some frame in the track length is in the boundingbox
                 if (x<=xEndLimit and x>xStartLimit and y<=yEndLimit and y>yStartLimit):
                     condition=condition and True 
@@ -36,13 +41,15 @@ def getTrajectories(filepath,startframe,endframe,trajectoylength,x,y,width,heigh
             print condition 
             #adding the trajectory completelyto the file
             if inc>=trajectoylength/2 or condition==True:
-               return line             
+                trajectoryList.append(linesplit[41:])
     f.close
+    return trajectoryList
 
 def getBagOfWords(codebook,data):
     #here it accepts the codebook and the trajectory to find to which bin this trajectory is close and assign the index
     #of the code book
-    binsize=len(codebook)
+    binSize=len(codebook)
+    print "binsize"+str(binSize)
     bagofwords=[0]*binSize 
     #there is a chance that the file may not have any trajectories which may lead to empty
     #data vector
@@ -55,8 +62,6 @@ def getBagOfWords(codebook,data):
         bagofwords=[float(x)/len(idx) for x in bagofwords]
     return bagofwords
 
-
-
 def getScoresForVideo(filepath,codebookpath,trajectoylength,x,y,w,h,boundingVolumeLength,stepspace,steptime):
     line = subprocess.check_output(['head', '-1', filepath])
     startframe=int(line.split('\t')[1])
@@ -65,6 +70,7 @@ def getScoresForVideo(filepath,codebookpath,trajectoylength,x,y,w,h,boundingVolu
     print startframe,endframe
     for frame in range(startframe,endframe-steptime,steptime):
         trajectory=getTrajectories(filepath,frame,frame+boundingVolumeLength,trajectoylength,x,y,w,h) 
+        print trajectory
         f=open(codebookpath)
         codebook=pickle.load(f)
         bow=getBagOfWords(codebook,trajectory)
@@ -114,10 +120,16 @@ def getEvaluations(datasets,save_location):
                 codebookpath='/home/kaushal/Documents/projects/dense_trajectory_and_codebook/data/results/codebooks/codebook100pickle.txt'
                 getScoresForVideo(full_dtfile_location,codebookpath,trajectoyLength,x,y,w,h,boundingVolumeLength,stepspace,steptime)
                 print x,y,w,h,startframe,endframe
-                break
        
 
 if __name__=="__main__":
-   datasets=('/home/kaushal/Documents/projects/dense_trajectory_and_codebook/data/groundtruth/MSR2','/home/kaushal/Documents/projects/dense_trajectory_and_codebook/data/groundtruth/KTH')
-   results_location='/home/kaushal/Documents/projects/dense_trajectory_and_codebook/data/results'
-   getEvaluations(datasets,results_location)
+    datasets=('/home/kaushal/Documents/projects/dense_trajectory_and_codebook/data/groundtruth/MSR2','/home/kaushal/Documents/projects/dense_trajectory_and_codebook/data/groundtruth/KTH')
+    results_location='/home/kaushal/Documents/projects/dense_trajectory_and_codebook/data/results'
+    #getEvaluations(datasets,results_location)
+    eg_filepath='/home/kaushal/Documents/projects/dense_trajectory_and_codebook/data/results/MSR2/boxing/10.avi.txt'
+    stepspace=3
+    steptime=5
+    trajectoyLength=15
+    codebookpath='/home/kaushal/Documents/projects/dense_trajectory_and_codebook/data/results/codebooks/codebook100pickle.txt'
+    print getTrajectories(eg_filepath,15,400,trajectoyLength,23,45,70,90) 
+        
